dc2e356953ba4ee5a88e45b0cd38c6000d1a1fb0
Liam Gallagher <liam@ibm.com>
2021-11-01 13:01:48 -0500

    added more tests

--- valid-parantheses.test.js
+++ valid-parantheses.test.js
@@ -1,7 +1,7 @@
 const { expect } = require('@jest/globals');
 const isValidParantheses = require('./valid-parantheses');
 
-test('Test true', () => {
+test('Test string true', () => {
   const testStr = '(()())';
 
   const result = isValidParantheses(testStr);
@@ -9,8 +9,39 @@ test('Test true', () => {
   expect(result).toBeTruthy();
 });
 
-test('Test false', () => {
+test('Test string false', () => {
   const testStr = '(()';
+
+  const result = isValidParantheses(testStr);
+
+  expect(result).toBeFalsy();
+});
+
+test('Test lisp code true', () => {
+  const testStr = `
+  (defun csg-intersection-intersect-all (obj-a obj-b)
+   (lambda (ray)
+     (flet ((inside-p (obj) (lambda (d) (inside-p obj (ray-point ray d)))))
+       (merge 'fvector
+              (remove-if-not (inside-p obj-b) (intersect-all obj-a ray))
+              (remove-if-not (inside-p obj-a) (intersect-all obj-b ray))
+              #'<))))`;
+
+  const result = isValidParantheses(testStr);
+
+  expect(result).toBeTruthy();
+});
+
+test('Test lisp code false', () => {
+  const testStr = `
+  (
+  (defun csg-intersection-intersect-all (obj-a obj-b)
+   (lambda (ray)
+     (flet ((inside-p (obj) (lambda (d) (inside-p obj (ray-point ray d)))))
+       (merge 'fvector
+              (remove-if-not (inside-p obj-b) (intersect-all obj-a ray))
+              (remove-if-not (inside-p obj-a) (intersect-all obj-b ray))
+              #'<))))`;
 
   const result = isValidParantheses(testStr);
 
