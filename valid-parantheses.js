83bcfc1a79a1b9c83174cd7da93cb1a57530a6ce
Liam Gallagher <liam@ibm.com>
2021-11-01 13:01:10 -0500

    finished validate-parantheses function

--- valid-parantheses.js
+++ valid-parantheses.js
@@ -1,3 +1,16 @@
-const isValidParantheses = (input) => {};
+const isValidParantheses = (input) => {
+  const stack = [];
+
+  for (const char of input) {
+    if (char === '(') {
+      stack.push(')');
+    } else if (char === ')' && stack.pop() !== char) {
+      return false;
+    }
+  }
+
+  // if stack isn't empty, parantheses are unclosed
+  return stack.length === 0;
+};
 
 module.exports = isValidParantheses;
